0b7be22c8f0d50f73483f304aacc27da65326944
Mohammad Kebbi <kebbi@ualberta.ca>
2019-05-10 19:42:05 -0600

    simple boot sector

---
+++ boot_sector.asm
@@ -0,0 +1,17 @@
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
+
+; padding
+times 510 - ($-$$) db 0
+;magic number
+dw 0xaa55
