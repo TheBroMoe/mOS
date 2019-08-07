9412c1637f1c94dade26950f0cd982e2045d5b5c
Mohammad Kebbi <kebbi@ualberta.ca>
2019-08-06 22:25:46 -0600

    finally

---
+++ path.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CC=/usr/bin/gcc
+export LD=/usr/bin/gcc
+export PREFIX="/usr/local/i686elfgcc"
+export TARGET=1686-elf
+export PATH="$PREFIX/bin:$PATH"
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y install nasm
+sudo apt -y install qemu
